cdd781b352a25fcf332bad4b81dc18c921428306
Philip Fernandez <philipfernandezz@gmail.com>
2022-12-21 17:51:40 -0500

    database

---
+++ database/db.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const { dbName, dbUser, dbPwd } = require("./utils/configDB");
+
+console.log("Opening database connection");
+
+const db = new Sequelize(dbName, dbUser, dbPwd, {
+  host: "localhost",
+  dialect: "postgres",
+  port: 5432,
+});
+
+module.exports = db;
